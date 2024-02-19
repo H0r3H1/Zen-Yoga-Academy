76e29e0db9afa2737205ea7d9d6135253f17d5b9
H0r3H1 <113664578+H0r3H1@users.noreply.github.com>
2024-02-19 16:33:22 +0530

    Add files via upload

---
+++ Frontend/DAY-1/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import SignInUpForm from './components/login.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SignInUpForm/>
+  </React.StrictMode>,
+)
